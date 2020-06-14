1a04bb93d4df76e5640cb9be00399d8f10c1956e
Joe Wills <JGW3Code@gmail.com>
2020-06-13 22:49:46 -0500

    another peice working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 var fs = require('fs');
 const path = require ('path');
 const generateMarkdown = require('./utils/generateMarkdown');
@@ -8,16 +8,19 @@ const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the name of this project?",
-
+    message: "What is the name of this project?"
+  }, 
+  {
     type: "input",
     name: "description",
-    message: "What is the purpose of this project?",
-    
+    message: "What is the purpose of this project?"
+  },
+  { 
     type: "input",
     name: "installation",
-    message: "How do you install this project?",
-
+    message: "How do you install this project?"
+  },
+  {
     type: "checkbox",
     name: "usage",
     message: "What language did you use for this project?",
@@ -28,7 +31,8 @@ const questions = [
         "Jquery",
         "Klingon"
     ]
-
+  },
+  {
     type: "checkbox",
     name: "license",
     message: "What license would you like to use on this project?",
@@ -39,27 +43,33 @@ const questions = [
         "Mozilla Public",
         "Apache"
     ]
-
+  },
+  {
     type: "input",
     name: "contributing",
-    message: "Who helped on this project?",
-
+    message: "Who helped on this project?"
+  },
+  {
     type: "input",
     name: "demo",
-    message: "Does this project have a demo?",
-    
+    message: "Does this project have a demo?"
+  },
+  {    
     type: "input", 
     name: "github", 
-    message: "What is your name on Github?",
-    
+    message: "What is your name on Github?"
+  },
+  {    
     type: "input", 
     name: "email", 
-    message: "What is your email address?",
-
+    message: "What is your email address?"
+  },
+  {
     type: "input", 
     name: "questions", 
-    message: "Do you have any questions about this project?",
-  }
+    message: "Do you have any questions about this project?"
+   },
+  
 ];
 
 // function to write README file
@@ -70,11 +80,10 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
     inquirer
-    .prompt(questions)
-      .then((data) => {
+    .prompt (questions)
+    .then((data) => {
         writeToFile ("README.md", generateMarkdown({...data}))
-      })
-        .catch(error => {
+      }).catch((err) => {
         console.log(err);
       })
 }
